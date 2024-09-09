2d596f5ed9570bf5bc906b73dfda9768edab43a8
Ravishankar <ravishankar.gnanaprakasam@gmail.com>
2024-09-09 16:12:29 +0530

    Mattermost Integration flow UI changes

---
+++ grafana-plugin/src/containers/MattermostIntegrationButton/MattermostIntegrationButton.tsx
@@ -0,0 +1,162 @@
+import React, { useCallback, useState } from 'react';
+
+import { Button, Modal, HorizontalGroup, VerticalGroup, Field, Input } from '@grafana/ui';
+import cn from 'classnames/bind';
+import { get, kebabCase } from 'lodash-es';
+import { observer } from 'mobx-react';
+import { Controller, FormProvider, useForm } from 'react-hook-form';
+
+import { WithPermissionControlTooltip } from 'containers/WithPermissionControl/WithPermissionControlTooltip';
+import { useStore } from 'state/useStore';
+import { UserActions } from 'utils/authorization/authorization';
+import { openErrorNotification } from 'utils/utils';
+
+import styles from './MattermostIntegrationButton.module.css';
+
+const cx = cn.bind(styles);
+
+interface MattermostIntegrationProps {
+  disabled?: boolean;
+  size?: 'md' | 'lg';
+  onUpdate: () => void;
+}
+export const MattermostIntegrationButton = observer((props: MattermostIntegrationProps) => {
+  const { disabled, size = 'md', onUpdate } = props;
+
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const onCreateModalCallback = useCallback(() => {
+    setShowModal(true);
+  }, []);
+
+  const onModalUpdateCallback = useCallback(() => {
+    setShowModal(false);
+
+    onUpdate();
+  }, [onUpdate]);
+
+  return (
+    <>
+      <WithPermissionControlTooltip userAction={UserActions.IntegrationsWrite}>
+        <Button size={size} variant="primary" icon="plus" disabled={disabled} onClick={onCreateModalCallback}>
+          Add Mattermost channel
+        </Button>
+      </WithPermissionControlTooltip>
+      {showModal && <MattermostChannelForm onHide={onCreateModalCallback} onUpdate={onModalUpdateCallback} />}
+    </>
+  );
+});
+
+interface MattermostCreationModalProps {
+  onHide: () => void;
+  onUpdate: () => void;
+}
+
+interface FormFields {
+  teamName: string;
+  channelName: string;
+}
+
+const MattermostChannelForm = (props: MattermostCreationModalProps) => {
+  const { onHide, onUpdate } = props;
+  const store = useStore();
+
+  const formMethods = useForm<FormFields>({
+    mode: 'onChange',
+  });
+
+  const {
+    control,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = formMethods;
+
+  const teamName = watch('teamName');
+  const channelName = watch('channelName');
+
+  return (
+    <Modal title="Add Mattermost Channel" isOpen closeOnEscape={false} onDismiss={onUpdate}>
+      <FormProvider {...formMethods}>
+        <form onSubmit={handleSubmit(onCreateChannelCallback)}>
+          <VerticalGroup>
+            {renderTeamNameInput()}
+            {renderChannelNameInput()}
+            <HorizontalGroup justify="flex-end">
+              <Button variant="secondary" onClick={() => onHide()}>
+                Cancel
+              </Button>
+              <Button type="submit" disabled={!teamName} variant="primary">
+                Create
+              </Button>
+            </HorizontalGroup>
+          </VerticalGroup>
+        </form>
+      </FormProvider>
+    </Modal>
+  );
+
+  function renderTeamNameInput() {
+    return (
+      <Controller
+        name="teamName"
+        control={control}
+        rules={{ required: 'Team Name is required' }}
+        render={({ field }) => (
+          <Field
+            label="Mattermost Team Name"
+            invalid={Boolean(errors['teamName'])}
+            error={errors['teamName']?.message}
+            className={cx('field')}
+          >
+            <Input
+              {...field}
+              className={cx('channelFormField__input')}
+              maxLength={50}
+              placeholder="Enter Mattermost Team Name"
+              autoFocus
+            />
+          </Field>
+        )}
+      />
+    );
+  }
+
+  function renderChannelNameInput() {
+    return (
+      <Controller
+        name="channelName"
+        control={control}
+        rules={{ required: 'Channel Name is required' }}
+        render={({ field }) => (
+          <Field
+            label="Mattermost Channel Name"
+            invalid={Boolean(errors['channelName'])}
+            error={errors['teamName']?.message}
+            className={cx('field')}
+          >
+            <Input
+              {...field}
+              className={cx('channelFormField__input')}
+              maxLength={50}
+              placeholder="Enter Mattermost Channel Name"
+              autoFocus
+            />
+          </Field>
+        )}
+      />
+    );
+  }
+
+  async function onCreateChannelCallback() {
+    try {
+      await store.mattermostChannelStore.create({
+        team_name: kebabCase(teamName),
+        channel_name: kebabCase(channelName),
+      });
+      onUpdate();
+    } catch (error) {
+      openErrorNotification(get(error, 'response.data.detail', 'error creating channel'));
+    }
+  }
+};
--- grafana-plugin/src/pages/settings/tabs/ChatOps/tabs/MattermostSettings/MattermostSettings.tsx
+++ grafana-plugin/src/pages/settings/tabs/ChatOps/tabs/MattermostSettings/MattermostSettings.tsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 
-import { Button, HorizontalGroup, VerticalGroup, Icon } from '@grafana/ui';
+import { Badge, Button, HorizontalGroup, LoadingPlaceholder, VerticalGroup } from '@grafana/ui';
 import cn from 'classnames/bind';
 import { observer } from 'mobx-react';
 
 import { Block } from 'components/GBlock/Block';
+import { GTable } from 'components/GTable/GTable';
 import { PluginLink } from 'components/PluginLink/PluginLink';
 import { Text } from 'components/Text/Text';
+import { WithConfirm } from 'components/WithConfirm/WithConfirm';
+import { MattermostIntegrationButton } from 'containers/MattermostIntegrationButton/MattermostIntegrationButton';
+import { MattermostChannel } from 'models/mattermost/mattermost.types';
 import { AppFeature } from 'state/features';
 import { WithStoreProps } from 'state/types';
 import { withMobXProviderContext } from 'state/withStore';
 import { DOCS_MATTERMOST_SETUP } from 'utils/consts';
-import { showApiError } from 'utils/utils';
 
 import styles from './MattermostSettings.module.css';
 
@@ -25,22 +28,23 @@ interface MattermostState {}
 class MattermostSettings extends Component<MattermostProps, MattermostState> {
   state: MattermostState = {};
 
-  handleOpenMattermostInstructions = async () => {
+  componentDidMount() {
+    this.update();
+  }
+
+  update = () => {
     const { store } = this.props;
-    try {
-      await store.mattermostStore.installMattermostIntegration();
-    } catch (err) {
-      showApiError(err);
-    }
+
+    store.mattermostChannelStore.updateItems();
   };
 
   render() {
     const { store } = this.props;
-    const { organizationStore } = store;
-    const envStatus = organizationStore.currentOrganization?.env_status.mattermost_configured;
-    const isIntegrated = false; // TODO: Check if integration is configured and can show channels view
+    const { mattermostChannelStore, organizationStore } = store;
+    const connectedChannels = mattermostChannelStore.getSearchResult();
+    const mattermostConfigured = organizationStore.currentOrganization?.env_status.mattermost_configured;
 
-    if (!isIntegrated) {
+    if (!mattermostConfigured && store.hasFeature(AppFeature.LiveSettings)) {
       return (
         <VerticalGroup spacing="lg">
           <Text.Title level={2}>Connect Mattermost workspace</Text.Title>
@@ -62,36 +66,128 @@ class MattermostSettings extends Component<MattermostProps, MattermostState> {
               </Text>
             </VerticalGroup>
           </Block>
-          {envStatus ? (
-            <HorizontalGroup>
-              <Button onClick={this.handleOpenMattermostInstructions}>
-                <HorizontalGroup spacing="xs" align="center">
-                  <Icon name="external-link-alt" className={cx('external-link-style')} /> Open Mattermost connection
-                  page
-                </HorizontalGroup>
-              </Button>
-              {store.hasFeature(AppFeature.LiveSettings) && (
-                <PluginLink query={{ page: 'live-settings' }}>
-                  <Button variant="secondary">See ENV Variables</Button>
-                </PluginLink>
-              )}
-            </HorizontalGroup>
-          ) : (
-            <HorizontalGroup>
-              <PluginLink query={{ page: 'live-settings' }}>
-                <Button variant="primary">Setup ENV Variables</Button>
-              </PluginLink>
-            </HorizontalGroup>
-          )}
-        </VerticalGroup>
-      );
-    } else {
-      return (
-        <VerticalGroup spacing="lg">
-          <Text.Title level={2}>Connected Mattermost workspace</Text.Title>
+          <PluginLink query={{ page: 'live-settings' }}>
+            <Button variant="primary">Setup ENV Variables</Button>
+          </PluginLink>
         </VerticalGroup>
       );
     }
+
+    if (!connectedChannels) {
+      return <LoadingPlaceholder text="Loading..." />;
+    }
+
+    if (!connectedChannels.length) {
+      return (
+        <VerticalGroup spacing="lg">
+          <Text.Title level={2}>Connect Mattermost workspace</Text.Title>
+          <Block bordered withBackground className={cx('mattermost-infoblock')}>
+            <VerticalGroup align="center">
+              <Text className={cx('infoblock-text')}>
+                Connecting Mattermost App will allow you to manage alert groups in your team Mattermost workspace.
+              </Text>
+
+              <Text className={cx('infoblock-text')}>
+                After a basic workspace connection your team members need to connect their personal Mattermost accounts
+                in order to be allowed to manage alert groups.
+              </Text>
+              <Text type="secondary" className={cx('infoblock-text')}>
+                More details in{' '}
+                <a href={DOCS_MATTERMOST_SETUP} target="_blank" rel="noreferrer">
+                  <Text type="link">our documentation</Text>
+                </a>
+              </Text>
+            </VerticalGroup>
+          </Block>
+          <HorizontalGroup>
+            <MattermostIntegrationButton size="md" onUpdate={this.update} />
+            {store.hasFeature(AppFeature.LiveSettings) && (
+              <PluginLink query={{ page: 'live-settings' }}>
+                <Button variant="primary">See ENV Variables</Button>
+              </PluginLink>
+            )}
+          </HorizontalGroup>
+        </VerticalGroup>
+      );
+    }
+
+    const columns = [
+      {
+        width: '70%',
+        title: 'Channel',
+        key: 'name',
+        render: this.renderChannelName,
+      },
+      {
+        width: '30%',
+        key: 'action',
+        render: this.renderActionButtons,
+      },
+    ];
+
+    return (
+      <div>
+        {connectedChannels && (
+          <div className={cx('root')}>
+            <GTable
+              title={() => (
+                <div className={cx('header')}>
+                  <Text.Title level={3}>Mattermost Channels</Text.Title>
+                  <MattermostIntegrationButton onUpdate={this.update} />
+                </div>
+              )}
+              emptyText={connectedChannels ? 'No Mattermost channels connected' : 'Loading...'}
+              rowKey="id"
+              columns={columns}
+              data={connectedChannels}
+            />
+          </div>
+        )}
+      </div>
+    );
   }
+
+  renderChannelName = (record: MattermostChannel) => {
+    return (
+      <>
+        {record.display_name} {record.is_default_channel && <Badge text="Default" color="green" />}
+      </>
+    );
+  };
+
+  renderActionButtons = (record: MattermostChannel) => {
+    return (
+      <HorizontalGroup justify="flex-end">
+        <Button
+          onClick={() => this.makeMattermostChannelDefault(record.id)}
+          disabled={record.is_default_channel}
+          fill="text"
+        >
+          Make default
+        </Button>
+        <WithConfirm title="Are you sure to disconnect?">
+          <Button onClick={() => this.disconnectMattermostChannel(record.id)} fill="text" variant="destructive">
+            Disconnect
+          </Button>
+        </WithConfirm>
+      </HorizontalGroup>
+    );
+  };
+
+  makeMattermostChannelDefault = async (id: MattermostChannel['id']) => {
+    const { store } = this.props;
+    const { mattermostChannelStore } = store;
+
+    await mattermostChannelStore.makeMattermostChannelDefault(id);
+    mattermostChannelStore.updateItems();
+  };
+
+  disconnectMattermostChannel = async (id: MattermostChannel['id']) => {
+    const { store } = this.props;
+    const { mattermostChannelStore } = store;
+
+    await mattermostChannelStore.deleteMattermostChannel(id);
+    mattermostChannelStore.updateItems();
+  };
 }
 export default withMobXProviderContext(MattermostSettings);
